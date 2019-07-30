69d149b29c939e36f41e79d905cb47f3edb2f449
Rick Jansen <rick.jansen1984@gmail.com>
2019-07-30 17:58:09 +0200

    Generate HDF5 file with datasets and attributes

---
+++ hdf5generator.py
@@ -0,0 +1,166 @@
+from h5py import Dataset
+
+import h5py
+import numpy as np
+import os
+
+
+def write_func(in_files, out_file, group, attributes):
+    """Write the HDF5 file based on the input files, 
+    group names and attributes.
+    
+    TODO: Make sure everything works with non-string data i.e. images etc.
+    
+    Arguments:
+        in_files: List of input file to add to the HDF5 file.
+        out_file: HDF5 output file.
+        group: The group structure.
+        attributes: Attributes to add to the HDF5 file.
+
+    Raises:
+        FileNotFoundError: The entered file does not exist.
+        RuntimeError: An error occured while generating the HDF5 file.
+    """
+    data_file = h5py.File(out_file, 'a')
+    try:
+        for group in groups:
+            for in_file in in_files:
+                if group.split('/')[-2] in in_file:
+                    try:
+                        with open(in_file) as ocf:
+                            data = ocf.read()
+                            str_type = h5py.new_vlen(str)
+                            data_file.create_dataset(
+                                group + in_file.split('/')[-1],
+                                data=data, shape=(1,),
+                                dtype=str_type
+                            )
+                    except FileNotFoundError:
+                        print(in_file, "not found")
+                else:
+                    pass
+    except RuntimeError:
+        pass
+    for k, v in attributes.items():
+        data_file.attrs[k] = v
+    with h5py.File(out_file,  "a") as f:
+        f['/Nanopore/PRIMUL/RB/RB01/assembly.fasta'].attrs["EDAM"] = "This should be FASTA and assembly EDAM IDs"
+
+
+def find_datasets(name, node):
+    """Read the nodes from the h5py visititems function
+    and check if the node is a dataset.
+    
+    TODO: See if this works with different array types.
+    FIXME: Add option for different array types.
+    
+    Arguments:
+        name: Name from h5py visititems.
+        node: The group node from h5py visititems.
+    """
+    if isinstance(node, h5py.Dataset):
+        print(node)
+        # write_dataset(node[:][0], name.split('/')[-2], name.split('/')[-1])
+    else:
+        print(node)
+
+
+def get_groups(name):
+    """Get the ISA structure based on the HDF5 group name.
+
+    TODO: Return ISA structure with group names.
+    TODO: Find out want we want with this functionality.
+
+    Arguments:
+        name: The name of the HDF5 group.
+    """
+    groups = {}
+    isa_structure = ["Project", "Investigation", "Study", "Assay"]
+    groupname = name.split('/')[-1]
+    groups[isa_structure[len(name.split('/'))-1]] = groupname
+
+
+def write_dataset(dataset, folder, out_file):
+    """Create files based on the available datasets in the HDF5 file.
+
+    Arguments:
+        dataset: Dataset from the HDF5 file.
+        folder: Directory to save the new dataset file.
+        out_file: Name of the new dataset file.
+    """
+    if not os.path.isdir(folder):
+        os.makedirs(folder)
+    with open(folder + '/' + out_file, 'w') as writefile:
+        writefile.write(dataset)
+
+
+def get_attr(hdf_file):
+    """Print all attributes.
+
+    Arguments:
+        hdf_file {[type]} -- [description]
+    """
+    with h5py.File(hdf_file, 'r') as f:
+        print(hdf_file, "has the following attributes:")
+        print()
+        all_attr = list(f.attrs)
+        for attr in all_attr:
+            print(attr + ":")
+            attrlist = f.attrs.get(attr)
+        for att in attrlist:
+            print(att)
+
+
+def delete_dataset(input, datasets_to_delete):
+    with h5py.File(input,  "a") as f:
+        for datasetname in datasets_to_delete:
+            del f[datasetname]
+
+
+if __name__ == "__main__":
+    in_files = [
+        'D:/Nanopore/Rick/results/RB01_Meta/assembly/RB01/assembly.fasta',
+        'D:/Nanopore/Rick/results/RB01_Meta/assembly/RB01/assembly_graph.gfa',
+        'D:/Nanopore/Rick/results/RB01_Meta/assembly/RB01/assembly_info.txt',
+        'D:/Nanopore/Rick/results/RB01_Meta/qc/RB01/RB01HistogramReadlength.png'
+    ]
+    groups = [
+        '/Nanopore/PRIMUL/RB/RB01/'
+    ]
+    options = input(
+        "Please enter the number of the option you want to use. 1=Create HDF5 file 2=Find datasets 3=Get attributes from HDF5 file 4=Delete datasets: ")
+    out_file = input("Enter HDF5 filename: ")
+    if int(options) == 1:
+        attributes = {}
+        while True:
+            attr = input(
+                "Enter an attributes i.e. EDAM http://edamontology.org/topic_3168,http://edamontology.org/format_1929: ")
+            if "," in attr:
+                attributes[attr.split(' ')[0]] = attr.split(' ')[1].split(',')
+            elif attr == '':
+                pass
+            else:
+                attributes[attr.split(' ')[0]] = [attr.split(' ')[1]]
+            continue_attr = input("Add another attribute? (Y/N): ")
+            if continue_attr == "y" or continue_attr == "Y":
+                True
+            else:
+                break
+        write_func(in_files, out_file,
+                   groups, attributes)
+    elif int(options) == 2:
+        with h5py.File(out_file, 'r') as f:
+            f.visititems(find_datasets)
+    elif int(options) == 3:
+        get_attr(out_file)
+    elif int(options) == 4:
+        datasets_to_delete = []
+        while True:
+            datasetname = input("Enter dataset name to delete: ")
+            datasets_to_delete.append(datasetname)
+            continue_del = input("Add another dataset to delete? (Y/N): ")
+            if continue_del == "y" or continue_del == "Y":
+                True
+            else:
+                break
+        delete_dataset(out_file, datasets_to_delete)
