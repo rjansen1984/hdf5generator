ded56e6a121f2bf4b9cd13949f643aa19f902054
rjansen1984 <rick.jansen1984@gmail.com>
2019-11-04 16:21:36 +0100

    Add Naturalis collection information to an HDF.

--- hdf5generator.py
+++ hdf5generator.py
@@ -2,13 +2,53 @@ from h5py import Dataset
 from PIL import Image
 from rdflib import Graph, URIRef, BNode, Literal, Namespace
 from rdflib.namespace import FOAF, RDF
+from subprocess import call
 
+import sys
 import h5py
 import numpy as np
 import os
 import ols_client
 import uuid
 import os.path
+import requests
+import json
+import datetime
+
+
+def write_groups(out_file, groupname):
+    """Write the groups and attributes to an HDF file.
+    
+    Arguments:
+        out_file: HDF5 output file.
+        groupname: Name of a group that will be added to the HDF.
+    """
+    print("To create a single group please just enter the main group name i.e. Group Name")
+    print('To create a subgroup to an exisitng group, please enter /Group Name/Subgroup Name/etc/etc/')
+    print()    
+    attributes = {}
+    print("Enter attributes for", groupname)
+    meta = input("Is there a metadata file? (Y/N): ")
+    if meta == "Y" or meta == "y":
+        metapath = input("Enter metadata file path: ")
+        with open(metapath, 'r') as metafile:
+            for line in metafile:
+                line = line.split('\t')
+                item = line[0].strip('\n')
+                value = line[-1].strip('\n')
+                if item in attributes.keys():
+                    attributes[item].append(value)
+                else:
+                    attributes[item] = [value]
+    else:
+        input_attributes = input("Enter an attribute followed by a value. i.e. Project Name: iknowit, Date: 04-11-2019: ")
+        for attribute in input_attributes.split(','):
+            attribute = attribute.split(':')
+            attributes[attribute[0].strip(' ')] = attribute[1].strip(' ')
+    data_file = h5py.File(out_file, 'a')
+    dset = data_file.create_group(groupname)
+    for k, v in attributes.items():
+        dset.attrs[k] = v
 
 
 def write_func(in_files, out_file, groups):
@@ -30,34 +70,33 @@ def write_func(in_files, out_file, groups):
     image_extensions = ['jpg', 'jpeg', 'png', 'bmp', 'tiff']
     count = 0
     try:
-        if len(groups) == len(in_files):
-            for in_file in in_files:
-                if in_file.split('.')[-1] not in image_extensions:
-                    try:
-                        with open(in_file) as ocf:
-                            data = ocf.read()
-                        str_type = h5py.special_dtype(vlen=str)
-                        dset = data_file.create_dataset(
-                            groups[count] + in_file.split('/')[-1],
-                            data=data, shape=(1,),
-                            dtype=str_type
-                        )
-                        attributes = generate_attributes_to_add(
-                            groups[count] + in_file.split('/')[-1])
-                        for k, v in attributes.items():
-                            dset.attrs[k] = v
-                    except FileNotFoundError:
-                        print(in_file, "not found")
-                else:
-                    dset = image_to_hdf5(in_file, data_file, groups[count])
+        for in_file in in_files:
+            if in_file.split('.')[-1] not in image_extensions:
+                try:
+                    with open(in_file) as ocf:
+                        data = ocf.read()
+                    str_type = h5py.special_dtype(vlen=str)
+                    dset = data_file.create_dataset(
+                        groups[count] + in_file.split('/')[-1],
+                        data=data, shape=(1,),
+                        dtype=str_type
+                    )
                     attributes = generate_attributes_to_add(
                         groups[count] + in_file.split('/')[-1])
                     for k, v in attributes.items():
                         dset.attrs[k] = v
-                if len(groups) == 1:
-                    count = 0
-                else:
-                    count += 1
+                except FileNotFoundError:
+                    print(in_file, "not found")
+            else:
+                dset = image_to_hdf5(in_file, data_file, groups[count])
+                attributes = generate_attributes_to_add(
+                    groups[count] + in_file.split('/')[-1])
+                for k, v in attributes.items():
+                    dset.attrs[k] = v
+            if len(groups) == 1:
+                count = 0
+            else:
+                count += 1
     except RuntimeError:
         pass
 
@@ -74,38 +113,55 @@ def generate_attributes_to_add(group_name):
         Dictionary of attributes. Key is the name and 
         the values are the description and iri.
     """
-    attributes = {}
-    while True:
-        print()
-        print("Enter attributes for " + group_name)
-        print("Please enter the attribute name and search term")
-        print("i.e. Format fasta")
-        attr = input("")
-        attr_name = attr.split(' ')[0]
-        ontolist = ontologies(attr.split(' ')[1])
-        print("ID -- Name -- Description -- IRI")
-        print()
-        for onto in enumerate(ontolist):
-            print(onto[0], "--", onto[1][0], "--",
-                  onto[1][1], "--", onto[1][2])
-        select_onto = input(
-            "Select an ontology description(s) to add (comma seperated): ")
-        if "," in select_onto:
-            added_onto_list = []
-            for select in select_onto.split(','):
-                added_onto = ontolist[int(select)]
-                added_onto_list.append(added_onto[1] + "--" + added_onto[2])
-            attributes[attr_name] = [added_onto_list]
-        elif select_onto == '':
-            pass
-        else:
-            added_onto = ontolist[int(select_onto)]
-            attributes[attr_name] = [added_onto[1] + "--" + added_onto[2]]
-        continue_attr = input("Add another attribute? (Y/N): ")
-        if continue_attr == "y" or continue_attr == "Y":
-            True
-        else:
-            break
+    attributes = dict()
+    print("Enter attributes for " + group_name)
+    meta = input("Is there a metadata file? (Y/N): ")
+    if meta == "Y" or meta == "y":
+        metapath = input("Enter metadata file path: ")
+        with open(metapath, 'r') as metafile:
+            for line in metafile:
+                line = line.split('\t')
+                item = line[0].strip('\n')
+                value = line[-1].strip('\n')
+                if item in attributes.keys():
+                    attributes[item].append(value)
+                else:
+                    attributes[item] = [value]
+    else:
+        while True:
+            print()
+            print("Enter attributes for " + group_name)
+            ontosearching = input("Search ontologies? Y/N: ")
+            if ontosearching == "Y" or ontosearching == "y":
+                print("Please enter the attribute name and search term")
+                print("i.e. Format fasta")
+                attr = input("")
+                attr_name = attr.split(' ')[0]
+                ontolist = ontologies(attr.split(' ')[1])
+                print("ID -- Name -- Description -- IRI")
+                print()
+                for onto in enumerate(ontolist):
+                    print(onto[0], "--", onto[1][0], "--",
+                        onto[1][1], "--", onto[1][2])
+                select_onto = input(
+                    "Select an ontology description(s) to add (comma seperated): ")
+                if select_onto == '':
+                    pass
+                else:
+                    for select in select_onto.split(','):
+                        added_onto = ontolist[int(select)]
+                        attributes[attr_name + "Description"] = [added_onto[1]]
+                        attributes[attr_name + "IRI"] = [added_onto[2]]
+            else:
+                naturalis_term = input("Add a Naturalis specimen ID: ")
+                naturalis = search_naturalis(naturalis_term)
+                for nitem, nvalue in naturalis.items():
+                    attributes[nitem] = [nvalue]
+            continue_attr = input("Add another attribute? (Y/N): ")
+            if continue_attr == "y" or continue_attr == "Y":
+                True
+            else:
+                break
     return attributes
 
 
@@ -213,8 +269,9 @@ def get_attr(hdf_file):
             print(dataset, "has the following attributes:")
             all_attr = list(f[dataset].attrs)
             for attr in all_attr:
-                attr_value = str(f[dataset].attrs.get(attr)[0])
-                print(attr + ": " + attr_value.split("--")[1])
+                for val in f[dataset].attrs.get(attr):
+                    attr_value = str(val)
+                    print(attr + ": " + attr_value)
             print()
             print()
 
@@ -230,7 +287,7 @@ def generate_rdf(hdf_file):
         print(hdf_file.split('/')[-1] + ".rdf already exists!")
         pass
     else:
-        print("Making First RDF file!!!")
+        print("Making RDF file...")
         open(rdf_file, 'a').close()
     datasets = []
     g = Graph().parse(rdf_file, format='turtle')
@@ -254,8 +311,8 @@ def generate_rdf(hdf_file):
             with open(rdf_file) as rdfile:
                 contents = rdfile.read()
                 for dataset in datasets:
-                    if 'file://' + rdf_file + '#' + dataset in contents:
-                        print('file://' + rdf_file + '#' + dataset, "is already in RDF")
+                    if rdf_file in contents:
+                        print(dataset, "is already in RDF")
                     else:
                         uid_str = uuid.uuid4().urn
                         identifier = uid_str[9:]
@@ -266,42 +323,42 @@ def generate_rdf(hdf_file):
                         all_attr = list(f[dataset].attrs)
                         g.add(
                             (
-                                URIRef('file://' + rdf_file + '#' + dataset),
+                                URIRef(hdf_file + "#" + dataset),
                                 URIRef(DCTERMS + 'isPartOf'),
                                 Literal(catalog)
                             )
                         )
                         g.add(
                             (
-                                URIRef('file://' + rdf_file + '#' + dataset),
+                                URIRef(hdf_file + "#" + dataset),
                                 URIRef(RDF + 'type'),
                                 URIRef(DCAT + 'Dataset')
                             )
                         )
                         g.add(
                             (
-                                URIRef('file://' + rdf_file + '#' + dataset),
+                                URIRef(hdf_file + "#" + dataset),
                                 URIRef(DCAT + 'dataset'),
                                 Literal(isa_tab[-1])
                             )
                         )
                         g.add(
                             (
-                                URIRef('file://' + rdf_file + '#' + dataset),
+                                URIRef(hdf_file + "#" + dataset),
                                 URIRef(DCAT + 'title'),
                                 Literal(isa_tab[-1].split('.')[0])
                             )
                         )
                         g.add(
                             (
-                                URIRef('file://' + rdf_file + '#' + dataset),
+                                URIRef(hdf_file + "#" + dataset),
                                 URIRef(DCAT + 'identifier'),
                                 Literal(identifier)
                             )
                         )
                         g.add(
                             (
-                                URIRef('file://' + rdf_file + '#' + dataset),
+                                URIRef(hdf_file + "#" + dataset),
                                 URIRef(DCTERMS + 'format'),
                                 Literal(isa_tab[-1].split('.')[-1])
                             )
@@ -310,19 +367,34 @@ def generate_rdf(hdf_file):
                             f[dataset].attrs.get(attr)
                             predicate = URIRef(
                                 HDF2RDF + attr.replace(" ",  "-"))
-                            attr_value = f[dataset].attrs.get(attr)[0].split("--")
-                            g.add(
-                                (
-                                    URIRef('file://' + rdf_file +
-                                           '#' + dataset),
-                                    predicate,
-                                    Literal(attr_value[1])
+                            try:
+                                attr_value = f[dataset].attrs.get(attr)
+                            except AttributeError:
+                                literal_object = Literal(attr_value[0])
+                            if len(attr_value) > 1:
+                                for value in attr_value:
+                                    literal_object = Literal(value)
+                                    g.add(
+                                        (
+                                            URIRef(hdf_file + "#" + dataset),
+                                            predicate,
+                                            literal_object
+                                        )
+                                    )
+                            else:
+                                literal_object = Literal(attr_value[0])
+                                g.add(
+                                    (
+                                        URIRef(hdf_file + "#" + dataset),
+                                        predicate,
+                                        literal_object
+                                    )
                                 )
-                            )
                             c += 1
                         g.parse(hdf_file.split('/')
                                 [-1] + ".rdf", format="turtle")
             g.serialize(destination=hdf_file.split('/')[-1] + ".rdf", format="turtle")
+            print("Finished!")
 
 
 def delete_groups(hdf_file, groups_to_delete):
@@ -388,27 +460,107 @@ def ontologies(tag):
     return ontolist
 
 
+def query_rdf(rdf_file, predicate):
+    """Query the generated RDF file based on a predicate
+    
+    TODO: Add option to query based on other search terms.
+
+    Arguments:
+        rdf_file: Path of the RDF file to query.
+        predicate: The entered predicate used to query the RDF.
+    """
+    g = Graph().parse(rdf_file, format='turtle')
+    for s,p,o in g:
+        if predicate in p:
+            name = s.split('#')[1]
+            print(p, "======", o)
+            print("Available in file", s.split('#')[1])
+            print()
+    with h5py.File(rdf_file[:-4], 'r') as f:
+        print(f[name][:])
+    
+
+def search_naturalis(query):
+    """Query the naturalis specimen collection to add as attributes to an HDF.
+    
+    Arguments:
+        query: Search term to retrieve the specimen information.
+
+    Returns:
+        A dictionary containing information from the naturalis collection.
+    """
+    naturalis = {}
+    get_specimen = "https://api.biodiversitydata.nl/v2/specimen/find/" + query
+    headers = {
+        'accept': 'application/json',
+        'charset': 'UTF-8',
+    }
+    naturalis_json = requests.get(get_specimen, headers=headers).content
+    jnl = json.loads(naturalis_json)
+    for item in jnl:
+        if item != "sourceSystem" or item != "identifications":
+            naturalis[item] = str(jnl[item])
+        if item == "identifications":
+            for subitem in jnl[item]:
+                for x in range(len(jnl[item])):
+                    for subitem in jnl[item][x]:
+                        naturalis[subitem] = str(jnl[item][x][subitem])
+    return naturalis
+
+
+def help():
+    """Printing the help text when user selected the --help option or 
+    enetered an option that does not exist.
+    """
+    print("This script can be used to generate HDF files and convert them to triples.")
+    print()
+    print("Usage:") 
+    print("Create group in HDF file --> python hdf5generator.py --create_group <HDF path>")
+    print("Create HDF file --> python hdf5generator.py --create_hdf <HDF path>")
+    print("Get datasets from HDF file --> python hdf5generator.py --get_datasets <HDF path>")
+    print("Get fttributes from HDF file --> python hdf5generator.py --get_attributes <HDF path>")
+    print("Delete groups from HDF file --> python hdf5generator.py --delete_groups <HDF path>")
+    print("Create RDF file --> python hdf5generator.py --create_rdf <HDF path>")
+    print("search RDF file --> python hdf5generator.py --search_rdf <HDF path>")
+
+
 if __name__ == "__main__":
-    options = input(
-        "1 = Create HDF file; 2 = Find datasets; 3 = Get attributes; 4 = Delete groups; 5 = Generate RDF: "
-    )
-    out_file = input("Enter HDF5 output path: ")
-    if int(options) == 1:
-        groups = []
-        input_files = input(
-            "Enter file paths (seperated by a space): ").replace('\\', '/')
-        input_groups = input(
-            "Enter groups (seperated by a space) i.e. /Project/Investigation/Study/Assay/: ")
-        in_files = input_files.split(' ')
-        groups = input_groups.split(' ')
-        write_func(in_files, out_file, groups)
-    elif int(options) == 2:
-        with h5py.File(out_file, 'r') as f:
-            f.visititems(find_datasets)
-    elif int(options) == 3:
-        get_attr(out_file)
-    elif int(options) == 4:
-        groups_to_delete = generate_groups_to_delete()
-        delete_groups(out_file, groups_to_delete)
-    elif int(options) == 5:
-        generate_rdf(out_file)
+    if len(sys.argv) > 2:
+        out_file = sys.argv[2]
+        if sys.argv[1] == "--create_group":
+            input_files = input(
+                "Enter file paths (seperated by a space): "
+            ).replace('\\', '/')
+            in_files = input_files.split(' ')
+            groupname = input("Enter new groupname: ")
+            write_groups(out_file, groupname)
+        elif sys.argv[1] == "--create_hdf":
+            call(["rm " + out_file], shell=True)
+            groups = []
+            input_files = input(
+                "Enter file paths (seperated by a space): "
+            ).replace('\\', '/')
+            input_groups = input(
+                "Enter groups (seperated by a space) i.e. /Project/Investigation/Study/Assay/: ")
+            in_files = input_files.split(' ')
+            groups = input_groups.split(' ')
+            write_func(in_files, out_file, groups)
+        elif sys.argv[1] == "--get_datasets":
+            with h5py.File(out_file, 'r') as f:
+                f.visititems(find_datasets)
+        elif sys.argv[1] == "--get_attributes":
+            get_attr(out_file)
+        elif sys.argv[1] == "--delete_groups":
+            groups_to_delete = generate_groups_to_delete()
+            delete_groups(out_file, groups_to_delete)
+        elif sys.argv[1] == "--create_rdf":
+            generate_rdf(out_file)
+        elif sys.argv[1] == "--search_rdf":
+            predicate = input("Enter predicate to search in RDF: ")
+            query_rdf(out_file, predicate)
+        else:
+            help()
+    else:
+        if sys.argv[1] == "--help":
+            help()
+    
